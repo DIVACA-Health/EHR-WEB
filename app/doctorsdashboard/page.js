1d9e18a21fe513b21dc65cb5b2365a4b55559c3e
minibot47 <toludairo534@gmail.com>
2025-09-07 04:08:35 +0100

    edits to dashboard doctor

--- app/doctorsdashboard/page.js
+++ app/doctorsdashboard/page.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Doctors from '../comp/doctorsdashboard'
+import { Suspense } from 'react'
 
 const page = () => {
   return (
-    <Doctors/>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Doctors/>
+    </Suspense>
+
   )
 }
 
